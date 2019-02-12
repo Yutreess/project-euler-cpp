b3399672cb17e85016849c3c05ef491ceb0aa677
Justus <justus.rho4@gmail.com>
2019-02-11 19:44:18 -0600

    Add files via upload

---
+++ euler-1.cpp
@@ -0,0 +1,7 @@
+// Name:        Justus Peterson-Rhodes
+// Date:        February 11, 2019
+// File:        euler-1.cpp
+// Description: Find the sum of all numbers below
+//              a given integer that are multiples of 3 or 5
+//
+//
