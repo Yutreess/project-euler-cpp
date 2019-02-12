4d087a694f88d30020018cc165312ca41b5faea7
Justus <justus.rho4@gmail.com>
2019-02-11 20:09:14 -0600

    Add files via upload

--- euler-1.cpp
+++ euler-1.cpp
@@ -5,3 +5,19 @@
 //              a given integer that are multiples of 3 or 5
 //
 //
+
+#include <iostream>
+
+void findMultiples(number)
+{
+
+}
+
+int main()
+{
+  int num, sumOfFactors;
+  std::cout << "Enter a number to sum: " << std::endl;
+  std::cin >> num;
+  std::cout << findMultiples(num);
+  return 0;
+}
