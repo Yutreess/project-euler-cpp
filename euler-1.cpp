9ea5f23d95a39dda1c2687672f02165057d48a6d
Justus <justus.rho4@gmail.com>
2019-02-16 09:17:49 -0600

    Finished

--- euler-1.cpp
+++ euler-1.cpp
@@ -8,9 +8,17 @@
 
 #include <iostream>
 
-void findMultiples(number)
+int findMultiples(int number)
 {
-
+  int sum = 0;
+  for (int i = 1; i <= number; i++)
+  {
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 int main()
